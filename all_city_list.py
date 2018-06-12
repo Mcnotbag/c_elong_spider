a5225e6ea524dfaa06fcfcc723f4b6b51b942f8c
Mcnotbag <332342731@qq.com>
2018-06-12 18:22:56 +0800

    4.7
    city写入csv

--- all_city_list.py
+++ all_city_list.py
@@ -1,21 +1,28 @@
 import json
 import string
-
+import csv
 import requests
 from redis import Redis
 
-# redis_server = Redis(host="111.230.34.217",port=6379,decode_responses=True)
-# # first_letter = string.ascii_uppercase
-# first_letter = "STUVWXYZ"
-# url = "http://m.elong.com/hotel/api/gethotelcitysbyletter?_rt=1527589494989&letter={id}"
-#
-# for i in first_letter:
-#     url = "http://m.elong.com/hotel/api/gethotelcitysbyletter?_rt=1527589494989&letter={id}".format(id=i)
-#     response = requests.get(url)
-#     html_json = json.loads(response.content.decode())
-#     for city in html_json:
-#         need_city_list = {}
-#         need_city_list[city["cityName"]] = city["cityId"]
-#         print(need_city_list)
-#         redis_server.lpush("elong_city", need_city_list)
+redis_server = Redis(host="111.230.34.217",port=6379,decode_responses=True)
+first_letter = string.ascii_uppercase
+url = "http://m.elong.com/hotel/api/gethotelcitysbyletter?_rt=1527589494989&letter={id}"
+
+for i in first_letter:
+    url = "http://m.elong.com/hotel/api/gethotelcitysbyletter?_rt=1527589494989&letter={id}".format(id=i)
+    response = requests.get(url)
+    html_json = json.loads(response.content.decode())
+    for city in html_json:
+        # 存入redis
+        # need_city_list = {}
+        # need_city_list[city["cityName"]] = city["cityId"]
+        # print(need_city_list)
+        # redis_server.lpush("elong_city", need_city_list)
+        # 存入csv文件
+        csv_need_city = [city["cityName"],city["cityId"]]
+
+        with open("elong_all_city.csv","a",encoding="utf-8") as f:
+            csv_write = csv.writer(f,dialect='excel')
+            csv_write.writerow(csv_need_city)
+
 
--- hot_city_list.py
+++ hot_city_list.py
@@ -1,3 +1,4 @@
+import csv
 import json
 import re
 
@@ -6,15 +7,23 @@ from redis import Redis
 
 # redis_server = Redis(host="111.230.34.217",port=6379,decode_responses=True)
 #
-# response = requests.get("http://m.elong.com/hotel/api/getlistfilter?_rt=1527502104816&cityid=2003")
-# html_json = json.loads(response.content.decode())
-# hot_city_list = html_json["hotCityList"]
-# hot_city_list = eval(hot_city_list)
-# for hot_city in hot_city_list:
-#     need_city_list = {}
-#     need_city_list[hot_city["cityName"]] = hot_city["cityId"]
-#     print(need_city_list)
-#     redis_server.lpush("elong_city",need_city_list)
+response = requests.get("http://m.elong.com/hotel/api/getlistfilter?_rt=1527502104816&cityid=2003")
+html_json = json.loads(response.content.decode())
+hot_city_list = html_json["hotCityList"]
+hot_city_list = eval(hot_city_list)
+for hot_city in hot_city_list:
+    # 存入 redis
+    # need_city_list = {}
+    # need_city_list[hot_city["cityName"]] = hot_city["cityId"]
+    # print(need_city_list)
+    # redis_server.lpush("elong_city",need_city_list)
+    # 存入csv 文件
+    csv_need_city = [hot_city["cityName"], hot_city["cityId"]]
+
+    with open("elong_hot_city.csv", "a", encoding="utf-8") as f:
+        csv_write = csv.writer(f, dialect='excel')
+        csv_write.writerow(csv_need_city)
+
 
 # city = redis_server.mget("elong_hot_city")
 # citys = redis_server.lpop("elong_hot_city")
